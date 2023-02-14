30c14e7e9d584ff0c0681a7fa0da13085ecd84f8
svillagracia <svillagracia@gmail.com>
2023-02-14 13:14:21 -0800

    function start and tests for generating a list of prime numbers, added TODO reminder to refactor isPrime

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const assert = require('assert');
 // Functionality
 
 // Check if a number is/isn't prime
+// TODO: This is brute force. Fix it.
 const isPrime = (n) => {
   if (n <= 1) return false;
   for (let i = 2; i < n; i++) {
@@ -19,10 +20,16 @@ const isPrime = (n) => {
   return true;
 };
 
+// Generate an array of prime numbers
+const generatePrimes = (n) => {};
+
 // Tests
 
-// Test for checking if a number is/isn't prime (isPrime)
-
+/**
+ *
+ * Test for checking if a number is/isn't prime (isPrime)
+ *
+ */
 // basic level checks
 assert.strictEqual(isPrime(2), true);
 assert.strictEqual(isPrime(3), true);
@@ -54,3 +61,22 @@ assert.strictEqual(isPrime(35), false);
 assert.strictEqual(isPrime(7879), true);
 assert.strictEqual(isPrime(15485867), true);
 assert.strictEqual(isPrime(32452843), true);
+
+/**
+ * Tests for generating a list of prime numbers (generatePrimes)
+ */
+assert.strictEqual(generatePrimes(3), [2, 3, 5]);
+assert.strictEqual(generatePrimes(5), [2, 3, 5, 7, 11]);
+assert.strictEqual(generatePrimes(10), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+assert.strictEqual(
+  generatePrimes(100),
+  [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+    157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
+    239, 241, 251, 257, 263, 269, 271, 277, 281, 293, 307, 311, 313, 317, 331,
+    337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421,
+    431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509,
+    521, 523, 541,
+  ]
+);
