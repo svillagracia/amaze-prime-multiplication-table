c67a5fca9145e50f8a16b9c8a8cc98536f44821f
svillagracia <svillagracia@gmail.com>
2023-02-14 17:27:53 -0800

    refactoring isPrime

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const assert = require('assert');
 // Functionality
 
 // Check if a number is/isn't prime
-// TODO: This is brute force. Fix it.
 const isPrime = (n) => {
   if (n <= 1) return false;
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
   }
   return true;
 };
